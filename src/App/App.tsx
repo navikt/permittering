d48531bb41dcf5d69e6c17c5413722cc33fccff7
hilde.heggstad <hilde.heggstad@bekk.no>
2020-03-14 15:07:12 +0100

    begynt med logg-inn-side

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './App.css';
+import './App.less';
 import {basename} from "./paths";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import LoggInn from "./LoggInn/LoggInn";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <div className="app">
         <Router basename={basename}>
           <Route exact path="/">
-            Hovedside
+            <LoggInn/>
           </Route>
           <Route exact path="/permitteringsskjema">
             Permetteringsskjema
---
+++ src/App/LoggInn/LoggInn.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { Ingress, Normaltekst, Sidetittel } from 'nav-frontend-typografi';
+
+import handshake from './handshake.svg';
+
+import './Logginn.less';
+import Hovedknapp from "nav-frontend-knapper/lib/hovedknapp";
+import Lenke from "nav-frontend-lenker";
+import LoggInnBanner from "./LoggInnBanner/LoggInnBanner";
+
+const LoggInn = () => {
+    const redirectTilLogin = () => {
+        /*if (environment.MILJO === 'prod-sbs' || environment.MILJO === 'dev-sbs') {
+            window.location.href = '/arbeidsforhold/redirect-til-login';
+        } else {
+            document.cookie = 'selvbetjening-idtoken =0123456789..*; path=/;';
+            window.location.href = '/arbeidsforhold/';
+        }
+        */
+    };
+
+
+    return (
+        <div className="innloggingsside">
+            <LoggInnBanner/>
+            <div className="innloggingsside__innhold">
+                <div className="innloggingsside__circle">
+                    <img src={handshake} className="handtrykkbilde" alt="bilde av håndtrykk" />
+                </div>
+                <Sidetittel className="innloggingsside__sidetittel">Send inn permitteringsskjema</Sidetittel>
+
+                <Ingress className="innloggingsside__ingress">
+                    Logg inn for å sende inn skjema
+                </Ingress>
+
+                <Hovedknapp className="innloggingsside__loginKnapp" onClick={redirectTilLogin}>
+                    Logg inn
+                </Hovedknapp>
+
+                <div className="innloggingsside__besok-ditt-nav">
+                    <Normaltekst>
+                        Ønsker du å se dine tjenester som privatperson?
+                        <p className="logg-inn-lenke">
+                            <Lenke href="https://www.nav.no/person/dittnav/">Logg inn på Ditt NAV</Lenke>
+                        </p>
+                    </Normaltekst>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoggInn;
