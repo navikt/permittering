6a6458a5a94e3f815b42eacb6f0927a09c6789de
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-16 16:34:25 +0100

    enda en endring i url

--- src/App/LoggInn/LoggInn.tsx
+++ src/App/LoggInn/LoggInn.tsx
@@ -12,9 +12,8 @@ const LoggInn = () => {
     const redirectTilLogin = () => {
         console.log("redirect logginn");
         if (environment.MILJO === 'prod-sbs' || environment.MILJO === 'dev-sbs') {
-            if (process.env.LOGIN_URL)
                 console.log("klarte å lese env param url");
-                window.location.href = window.location.href = '/redirect-til-login';
+                window.location.href = '/permittering/redirect-til-login';
         }else{
             console.log("er ikke i miljø");
             document.cookie = 'selvbetjening-idtoken =0123456789..*; path=/;';
