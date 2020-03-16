f342220d6a0e6bd91dd126c38e6fc25c999ddbef
hilde.heggstad <hilde.heggstad@bekk.no>
2020-03-16 12:40:34 +0100

    forenklet mer

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,6 @@ const apiProxy = require('./routes/apiProxy');
 const BASE_PATH = '/permittering';
 const veilarbStatusProxyConfig = require('./veilarbStatusProxyConfig');
 
-const server = express();
-
 app.use(`${BASE_PATH}/veilarbstepup/status`, veilarbStatusProxyConfig);
 
 const startServer = (app, port) => {
@@ -24,23 +22,14 @@ const startServer = (app, port) => {
 
 const startMockServer = html => {
     console.log('start server');
-    app.use(BASE_PATH, express.static(buildPath));
-    setInternalEndpoints();
-    server.get(`${BASE_PATH}/*`, (req, res) => {
-        res.sendFile(path.resolve(buildPath, 'index.html'));
-    });
-    server.listen(port, () => {
+    loginRoutes(app);
+    apiProxy(app);
+    internalRoutes(app);
+    indexRoute(app);
+    app.listen(port, () => {
         console.log('Server listening on port', port);
     });
 };
-
-server.get(`${BASE_PATH}/redirect-til-login`, (req, res) => {
-  const loginUrl =
-      process.env.LOGIN_URL ||
-      'http://localhost:8080/permittering-api/local/selvbetjening-login?redirect=http://localhost:3000/permittering';
-  res.redirect(loginUrl);
-});
-
 
 if (process.env.REACT_APP_MOCK) {
   startMockServer();
