a402c0fea5c5c80f7b976fa062fc9777b02b6176
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-18 17:14:54 +0100

    la på slasker

--- src/server/envSettings.js
+++ src/server/envSettings.js
@@ -1,6 +1,6 @@
 function createEnvSettingsFile() {
   return `window.appSettings = {
-            MILJO: ${process.env.NAIS_CLUSTER_NAME},
+            MILJO: "${process.env.NAIS_CLUSTER_NAME}",
         };`;
 }
 module.exports = createEnvSettingsFile;
