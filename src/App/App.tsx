61f5f372e56e3bd7b84394f71e192c912715ac90
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-20 00:31:47 +0100

    forsøk å deploye til prod

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -59,7 +59,6 @@ function App() {
                                         <Kvitteringsside />
                                     </Route>
                                 </>{' '}
-                                )}
                             </FeatureToggleProvider>
                         </OrganisasjonsListeProvider>
                     </Router>
