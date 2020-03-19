01864f458dd3df1a51d981f0d3f04d4cfe41a54f
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-19 12:21:14 +0100

    fiks styling dropdown

--- src/App/komponenter/Bedriftsvelger/Bedriftsvelger.tsx
+++ src/App/komponenter/Bedriftsvelger/Bedriftsvelger.tsx
@@ -8,24 +8,22 @@ interface Props {
 
 export const BedriftsVelger: FunctionComponent<Props> = props => {
     return (
-        <div>
-            <select
-                className={'hva-skal-du-rapportere__bedriftsDropdown'}
-                onChange={event => {
-                    props.setOrganisasjon(event.target.value);
-                }}
-            >
-                {props.organisasjoner.map(organisasjon => {
-                    return (
-                        <option
-                            key={organisasjon.OrganizationNumber}
-                            value={organisasjon.OrganizationNumber}
-                        >
-                            {organisasjon.Name} - {organisasjon.OrganizationNumber}
-                        </option>
-                    );
-                })}
-            </select>
-        </div>
+        <select
+            className={'hva-skal-du-rapportere__bedriftsDropdown'}
+            onChange={event => {
+                props.setOrganisasjon(event.target.value);
+            }}
+        >
+            {props.organisasjoner.map(organisasjon => {
+                return (
+                    <option
+                        key={organisasjon.OrganizationNumber}
+                        value={organisasjon.OrganizationNumber}
+                    >
+                        {organisasjon.Name} - {organisasjon.OrganizationNumber}
+                    </option>
+                );
+            })}
+        </select>
     );
 };
