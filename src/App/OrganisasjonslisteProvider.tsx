3235f0c2dc00be644a542e6461cf24ed631a646b
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-19 13:29:10 +0100

    Legg valgt bedrift i bedriftskontekst (#82)
    * legg til dropdown på startside
    
    * la på styling
    
    * flytt bedriftsvelgerdropdown ut i egen komponent
    
    * fiks styling dropdown
    
    * fiks evig loop

--- src/App/OrganisasjonslisteProvider.tsx
+++ src/App/OrganisasjonslisteProvider.tsx
@@ -11,6 +11,8 @@ export enum Tilgang {
 export type OrganisajonsContext = {
     organisasjoner: Array<Organisasjon>;
     organisasjonslisteFerdigLastet: Tilgang;
+    valgtOrganisasjon: null | Organisasjon;
+    endreOrganisasjon: (org: Organisasjon) => void;
 };
 
 const OrganisasjonsListeContext = React.createContext<OrganisajonsContext>(
@@ -23,6 +25,14 @@ export const OrganisasjonsListeProvider: FunctionComponent = props => {
     const [organisasjonslisteFerdigLastet, setOrganisasjonslisteFerdigLastet] = useState(
         Tilgang.LASTER
     );
+    const [valgtOrganisasjon, setValgtOrganisasjon] = useState<null | Organisasjon>(null);
+
+    const endreOrganisasjon = async (org?: Organisasjon) => {
+        console.log('endre org kallt');
+        if (org) {
+            await setValgtOrganisasjon(org);
+        }
+    };
 
     useEffect(() => {
         const abortController = new AbortController();
@@ -31,12 +41,11 @@ export const OrganisasjonsListeProvider: FunctionComponent = props => {
             .then(organisasjonsliste => {
                 setOrganisasjoner(
                     organisasjonsliste.filter(
-                        organisasjon =>
-                            organisasjon.OrganizationForm === 'BEDR' ||
-                            organisasjon.Type === 'Enterprise'
+                        organisasjon => organisasjon.OrganizationForm === 'BEDR'
                     )
                 );
-                if (organisasjoner.length > 0) setOrganisasjonslisteFerdigLastet(Tilgang.TILGANG);
+                if (organisasjonsliste.length > 0)
+                    setOrganisasjonslisteFerdigLastet(Tilgang.TILGANG);
                 else {
                     setOrganisasjonslisteFerdigLastet(Tilgang.IKKE_TILGANG);
                 }
@@ -45,11 +54,13 @@ export const OrganisasjonsListeProvider: FunctionComponent = props => {
                 setOrganisasjoner([]);
                 //setVisFeilmelding(true);
             });
-    }, [organisasjoner]);
+    }, []);
 
     let defaultContext: OrganisajonsContext = {
         organisasjoner,
         organisasjonslisteFerdigLastet,
+        valgtOrganisasjon,
+        endreOrganisasjon,
     };
     return (
         <>
---
+++ src/App/komponenter/Bedriftsvelger/Bedriftsvelger.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from 'react';
+import { Organisasjon } from '@navikt/bedriftsmeny/lib/Organisasjon';
+
+interface Props {
+    organisasjoner: Array<Organisasjon>;
+    setOrganisasjon: (orgnr: string) => void;
+}
+
+export const BedriftsVelger: FunctionComponent<Props> = props => {
+    return (
+        <select
+            className={'hva-skal-du-rapportere__bedriftsDropdown'}
+            onChange={event => {
+                props.setOrganisasjon(event.target.value);
+            }}
+        >
+            {props.organisasjoner.map(organisasjon => {
+                return (
+                    <option
+                        key={organisasjon.OrganizationNumber}
+                        value={organisasjon.OrganizationNumber}
+                    >
+                        {organisasjon.Name} - {organisasjon.OrganizationNumber}
+                    </option>
+                );
+            })}
+        </select>
+    );
+};
