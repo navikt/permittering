5646ad7e117dca61903d192a9f552cf2b001aad0
Terese Haug <terese.haug@nav.no>
2020-03-22 16:09:52 +0100

    Fikser datoformat på oppsummering

---
+++ src/App/Skjema/Side4-oppsummering/oppsummering-utils.ts
@@ -0,0 +1,45 @@
+export const lagTekstVarighet = (type: string, fraEllerTil: string) => {
+    switch (true) {
+        case type === 'MASSEOPPSIGELSE':
+            return `Oppsigelsene ${fraEllerTil}`;
+        case type === 'PERMITTERING_UTEN_LØNN':
+            return `Permitteringene ${fraEllerTil}`;
+        case type === 'INNSKRENKNING_I_ARBEIDSTID':
+            return `Innskrenkningen i arbeidstid ${fraEllerTil}`;
+    }
+    return `Permitteringene ${fraEllerTil}`;
+};
+
+const finnMaaned = (month: number): string => {
+    switch (month) {
+        case 0:
+            return 'januar';
+        case 1:
+            return 'februar';
+        case 2:
+            return 'mars';
+        case 3:
+            return 'april';
+        case 4:
+            return 'mai';
+        case 5:
+            return 'juni';
+        case 6:
+            return 'juli';
+        case 7:
+            return 'august';
+        case 8:
+            return 'september';
+        case 9:
+            return 'oktober';
+        case 10:
+            return 'november';
+        case 11:
+            return 'desember';
+    }
+    return (month + 1).toString();
+};
+
+export const formatterDato = (dato: Date) => {
+    return dato.getDate() + '. ' + finnMaaned(dato.getMonth()) + ' ' + dato.getFullYear();
+};
