2e84a1ccce4f6fddf1e5381deabae29cb2398a6f
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-23 13:54:49 +0100

    Legg til tekst paa oppsummeringsside (#157)
    * la inn tkest på oppsummeringssiden
    
    * fjernet hele som-arbeidsgiver-må-du-boksen

--- src/App/Skjema/Side4-oppsummering/Oppsummering.tsx
+++ src/App/Skjema/Side4-oppsummering/Oppsummering.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Hovedknapp from 'nav-frontend-knapper/lib/hovedknapp';
-import { Normaltekst, Systemtittel, Undertittel } from 'nav-frontend-typografi';
+import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import Lenke from 'nav-frontend-lenker';
 import Knapp from 'nav-frontend-knapper/lib/knapp';
 import Veilederpanel from 'nav-frontend-veilederpanel';
@@ -10,7 +10,6 @@ import SkjemaRamme from '../../komponenter/SkjemaRamme';
 import { splittOppFritekst } from '../../../utils/fritekstFunksjoner';
 import { forrigeSide, SkjemaSideProps, skjemaSteg } from '../skjema-steg';
 import veilederIkon from './gjenstand.svg';
-import infoIkon from './info.svg';
 import Banner from '../../HovedBanner/HovedBanner';
 import { formatterDato, lagTekstBasertPaSkjemaType, lagTekstVarighet } from './oppsummering-utils';
 import './Oppsummering.less';
@@ -208,19 +207,6 @@ const Oppsummering: FunctionComponent<SkjemaSideProps> = () => {
                                 </Lenke>
                             </div>
                         </div>
-                    </Veilederpanel>
-
-                    <Veilederpanel
-                        type="plakat"
-                        kompakt
-                        fargetema="info"
-                        svg={<img src={infoIkon} alt="" aria-hidden="true" />}
-                    >
-                        <Undertittel>Som arbeidsgiver må du:</Undertittel>
-                        <ul>
-                            <li>Bla bla bla</li>
-                            <li>Bla bla bla</li>
-                        </ul>
                     </Veilederpanel>
 
                     <div className="skjema-innhold__fram-og-tilbake">
