25321c675499b6626c9ef1bd7ebef1dd81213361
hilde.heggstad <hilde.heggstad@bekk.no>
2020-03-26 14:46:25 +0100

    lagd ganske grei mobvisning

--- src/App/Forside/oversiktForMobil/oversiktForMobil.tsx
+++ src/App/Forside/oversiktForMobil/oversiktForMobil.tsx
@@ -3,9 +3,9 @@ import React, { FunctionComponent } from 'react';
 import './oversiktForMobil.less';
 import { Permitteringsskjema } from '../../../types/permitteringsskjema';
 import TidligereInnsendtSkjema from './TidligereInnsendtSkjema/tidligereInnsendtSkjema';
+import Undertittel from 'nav-frontend-typografi/lib/undertittel';
 
 interface Props {
-    className?: string;
     listeMedSkjema: any;
 }
 
@@ -13,7 +13,15 @@ const OversiktForMobil: FunctionComponent<Props> = (props: Props) => {
     const rader = props.listeMedSkjema.map((skjema: Permitteringsskjema) => (
         <TidligereInnsendtSkjema key={skjema.id} tidligereSkjema={skjema} />
     ));
-    return <ul className={props.className}>{rader}</ul>;
+    return (
+        <div className={'forside__mobilvisning'}>
+            <Undertittel className={'forside__mobilvisning__undertittel'}>
+                {' '}
+                Dine skjema{' '}
+            </Undertittel>
+            <ul className={'forside__mobilvisning__ul'}>{rader}</ul>
+        </div>
+    );
 };
 
 export default OversiktForMobil;
