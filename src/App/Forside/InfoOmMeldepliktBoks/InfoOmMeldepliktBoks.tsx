fe82503f40b09caa244575445999e46c59bf4fb6
hilde.heggstad <hilde.heggstad@bekk.no>
2020-03-26 13:28:24 +0100

    flyttet infojuridiskboks og fjernet hvitboks rammen

---
+++ src/App/Forside/InfoOmMeldepliktBoks/InfoOmMeldepliktBoks.tsx
@@ -0,0 +1,59 @@
+import React, { FunctionComponent } from 'react';
+
+import './InfoOmMeldepliktBoks.less';
+import infoIkon from '../info.svg';
+import { Element, Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+
+import { EksternLenke } from '../EksternLenke';
+import { Hovedknapp } from 'nav-frontend-knapper';
+import Lenke from 'nav-frontend-lenker';
+import { useHistory } from 'react-router';
+
+interface Props {
+    visskjema: any;
+}
+
+const InfoOmMeldepliktBoks: FunctionComponent<Props> = props => {
+    const history = useHistory();
+
+    return (
+        <div className={'forside__info-om-meldeplikt-boks'}>
+            <div>
+                <img src={infoIkon} alt="" aria-hidden="true" />
+            </div>
+            <div className={'forside__info-om-meldeplikt-boks__innhold'}>
+                <Systemtittel>Arbeidsgivers meldeplikt til NAV</Systemtittel>
+                <div className={'forside__info-om-meldeplikt-boks__innhold__ingress'}>
+                    <Normaltekst>
+                        Skal du permittere, si opp eller innskrenke arbeidstiden til 10 eller flere
+                        ansatte? Da har du meldeplikt til NAV. Du kan også melde ifra til NAV dersom
+                        det gjelder færre enn 10 ansatte om du ønsker det.
+                    </Normaltekst>
+                </div>
+                <Element>Dette er bestemt av:</Element>
+                <div>
+                    <Lenke href={'https://lovdata.no/lov/2004-12-10-76/§8'}>
+                        <span>Arbeidsmarkedsloven §8</span> <EksternLenke />{' '}
+                    </Lenke>
+                </div>
+                <div>
+                    <Lenke href={'https://lovdata.no/lov/2005-06-17-62/§15-2'}>
+                        <span>Arbeidsmiljøloven §15-2 </span> <EksternLenke />{' '}
+                    </Lenke>
+                </div>
+                <br />
+                {props.visskjema && (
+                    <div
+                        className={'forside__info-om-meldeplikt-boks__innhold__meld-til-nav-knapp'}
+                    >
+                        <Hovedknapp onClick={() => history.push('skjema/start')}>
+                            Meld fra til NAV
+                        </Hovedknapp>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default InfoOmMeldepliktBoks;
