2772da47342925fcb118a58afbaa28b685d97388
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-03-27 09:47:08 +0100

    la til unorm polyfil

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 import 'core-js';
-//import 'unorm/lib/unorm';
+import 'unorm/lib/unorm';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
