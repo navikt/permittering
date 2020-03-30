82539de7541768f8c13064b746df7d9c9a01d5aa
John Arne Føreland <john.foreland@me.com>
2020-03-30 12:00:43 +0200

    La til kilde for regex

--- src/App/Skjema/Side1/inputFeltValideringer.tsx
+++ src/App/Skjema/Side1/inputFeltValideringer.tsx
@@ -10,7 +10,8 @@ export const erGyldigTelefonNr = (nr: string) => {
 };
 
 export const erGyldigEpost = (epost: string) => {
-   
+
+    // kilde for regex https://emailregex.com (RFC 5322 Official Standard)
     const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     const isValidEmail = regexp.test(epost);
     
