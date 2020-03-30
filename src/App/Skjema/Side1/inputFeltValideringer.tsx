236e1cc11f3fc0b88e9dea67db63b2958f24845b
John Arne Føreland <john.foreland@me.com>
2020-03-30 10:22:43 +0200

    La til const på variabel

--- src/App/Skjema/Side1/inputFeltValideringer.tsx
+++ src/App/Skjema/Side1/inputFeltValideringer.tsx
@@ -11,7 +11,7 @@ export const erGyldigTelefonNr = (nr: string) => {
 
 export const erGyldigEpost = (epost: string) => {
    
-    regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     isValidEmail = regexp.test(epost);
     
     return isValidEmail;
