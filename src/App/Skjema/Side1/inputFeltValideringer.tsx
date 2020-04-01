194855ad6b166622e3fa4a3e81df46d112bd6e80
hilde.heggstad <hilde.heggstad@bekk.no>
2020-04-01 11:58:00 +0200

    lagt til info om jur vs virksomhet

--- src/App/Skjema/Side1/inputFeltValideringer.tsx
+++ src/App/Skjema/Side1/inputFeltValideringer.tsx
@@ -10,10 +10,12 @@ export const erGyldigTelefonNr = (nr: string) => {
 };
 
 export const erGyldigEpost = (epost: string) => {
-
     // kilde for regex https://emailregex.com (RFC 5322 Official Standard)
-    const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    // eslint-disable-next-line
+    const regexp = new RegExp(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
     const isValidEmail = regexp.test(epost);
-    
+
     return isValidEmail;
 };
