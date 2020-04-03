c5762686d3684d3855d7916c977f3b4c2c5a72b3
Hilde Steinbru Heggstad <47525174+hildeheggstad@users.noreply.github.com>
2020-04-03 14:25:17 +0200

    Fjern brodsmule pa noen sider (#295)
    * fjerna brodsmulesti
    
    * satt tilbake margin
    
    * avbrytknapp

--- src/App/komponenter/BrodsmuleSti/BrodsmuleSti.tsx
+++ src/App/komponenter/BrodsmuleSti/BrodsmuleSti.tsx
@@ -7,21 +7,21 @@ import SkjemaContext from '../../SkjemaContext/SkjemaContext';
 import environment from '../../../utils/environment';
 import { Permitteringsskjema } from '../../../types/permitteringsskjema';
 
+export const lagLinkTilMinSide = (skjema: Permitteringsskjema) => {
+    const bedriftsnummer = skjema && skjema.bedriftNr ? skjema.bedriftNr : '';
+    if (environment.MILJO === 'dev-sbs') {
+        return 'https://arbeidsgiver-q.nav.no/min-side-arbeidsgiver/' + bedriftsnummer;
+    }
+    return 'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/' + bedriftsnummer;
+};
+
 export const BrodsmuleSti = () => {
     const context = useContext(SkjemaContext);
     const [link, setlink] = useState('');
 
     useEffect(() => {
-        setlink(lagLink(context.skjema));
+        setlink(lagLinkTilMinSide(context.skjema));
     }, [context.skjema]);
-
-    const lagLink = (skjema: Permitteringsskjema) => {
-        const bedriftsnummer = skjema && skjema.bedriftNr ? skjema.bedriftNr : '';
-        if (environment.MILJO === 'dev-sbs') {
-            return 'https://arbeidsgiver-q.nav.no/min-side-arbeidsgiver/' + bedriftsnummer;
-        }
-        return 'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/' + bedriftsnummer;
-    };
 
     return (
         <Lenke className={'app__brodsmulesti'} href={link}>
