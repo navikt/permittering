af9f4ca009c54ed03b0aa20c4e81b33e1dc83d78
Terese Haug <terese.haug@nav.no>
2020-04-06 13:19:10 +0200

    Legger på dekoratør

--- src/server/routes/indexPath.js
+++ src/server/routes/indexPath.js
@@ -2,12 +2,13 @@ const paths = require('../../paths');
 const path = require('path');
 const express = require('express');
 const buildPath = path.join(__dirname, '../../../build');
-module.exports = app => {
-    app.use(paths.basePath, express.static(buildPath));
+
+module.exports = (app, html) => {
+    app.use(paths.basePath, express.static(buildPath, { index: false }));
     app.get('/', (req, res) => {
         res.redirect(301, paths.basePath);
     });
     app.get(`${paths.basePath}/*`, (req, res) => {
-        res.sendFile(path.resolve(buildPath, 'index.html'));
+        res.send(html);
     });
 };
