461e2b460433eacfcfce9446e9b66a6dc70c2db1
Terese Haug <terese.haug@nav.no>
2020-04-07 14:28:39 +0200

    Fikser feil i Heroku/dekororatør

--- src/server/heroku.js
+++ src/server/heroku.js
@@ -1,40 +1,18 @@
-const express = require('express');
-const app = express();
-const mustacheExpress = require('mustache-express');
-const getDecorator = require('./routes/decorator');
+const app = require('express')();
 const internalRoutes = require('./routes/internals');
-const indexRoute = require('./routes/indexPath');
+const indexRouteHeroku = require('./routes/indexPathHeroku');
 const loginRoutes = require('./routes/login');
 const apiMock = require('./routes/apiMock');
-const stillingstitlerMock = require('./routes/stillingstitlerMock');
 const settingsJsRoutes = require('./routes/settingsJs');
-const veilarbstepupRoutes = require('./routes/veilarbstepup');
-const port = process.env.PORT || 3000;
-const path = require('path');
-const buildPath = path.join(__dirname, '../../build');
-app.engine('html', mustacheExpress());
-app.set('view engine', 'mustache');
-app.set('views', buildPath);
+const stillingstitlerMock = require('./routes/stillingstitlerMock');
 
-const renderApp = decoratorFragments =>
-    new Promise((resolve, reject) => {
-        app.render('index.html', decoratorFragments, (err, html) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(html);
-            }
-        });
-    });
-
-const startServer = html => {
+const startServer = (app, port) => {
     console.log('start heroku server');
     settingsJsRoutes(app);
-    veilarbstepupRoutes(app);
     loginRoutes(app);
     apiMock(app);
     internalRoutes(app);
-    indexRoute(app, html);
+    indexRouteHeroku(app);
     stillingstitlerMock(app);
     app.listen(port, () => {
         console.log('Server listening on port', port);
@@ -47,13 +25,4 @@ const startServer = html => {
  * @param app
  * @param port
  */
-
-getDecorator()
-    .then(renderApp, error => {
-        console.error('Kunne ikke hente dekoratør ', error);
-        process.exit(1);
-    })
-    .then(startServer, error => {
-        console.error('Kunne ikke rendre app ', error);
-        process.exit(1);
-    });
+startServer(app, process.env.PORT || 3000);
