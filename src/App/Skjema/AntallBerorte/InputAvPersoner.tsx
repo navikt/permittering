0e1b45fe86e3c53f0404098ecbd6bf44e1171746
hilde.heggstad <hilde.heggstad@bekk.no>
2020-04-15 12:52:11 +0200

    tabellen reagerer pa endring av juridisk enhet

--- src/App/Skjema/AntallBerorte/InputAvPersoner.tsx
+++ src/App/Skjema/AntallBerorte/InputAvPersoner.tsx
@@ -13,14 +13,23 @@ import Lenke from 'nav-frontend-lenker';
 import 'nav-frontend-tabell-style';
 import { OrganisasjonsListeContext } from '../../OrganisasjonslisteProvider';
 import Input from 'nav-frontend-skjema/lib/input';
+import { BedriftsVelger } from '../../komponenter/Bedriftsvelger/Bedriftsvelger';
+import { tomAltinnOrganisasjon } from '../../../types/Organisasjon';
 
 const AntallBerorte: FunctionComponent = () => {
     const { organisasjonstre } = useContext(OrganisasjonsListeContext);
     const history = useHistory();
     const context = useContext(SkjemaContext);
     const [aktivStatusForRader, setAktivStatusForRader] = useState([]);
+    const [juridiskEnhetOrgnr, setJuridiskEnhetOrgnr] = useState('');
 
     const { steg } = useSkjemaSteg(history.location.pathname, context.skjema.id);
+
+    useEffect(() => {
+        if (organisasjonstre && organisasjonstre.length) {
+            setJuridiskEnhetOrgnr(organisasjonstre[0].JuridiskEnhet.OrganizationNumber);
+        }
+    }, [organisasjonstre]);
 
     useEffect(() => {
         if (organisasjonstre && organisasjonstre.length) {
@@ -31,18 +40,20 @@ const AntallBerorte: FunctionComponent = () => {
     }, [organisasjonstre]);
 
     let rader: any = [];
-    if (organisasjonstre && organisasjonstre.length > 0) {
-        rader = organisasjonstre[0].Underenheter.map((org, index) => {
+    if (juridiskEnhetOrgnr.length > 0 && organisasjonstre) {
+        const valgtEnhet = organisasjonstre.filter(
+            org => org.JuridiskEnhet.OrganizationNumber === juridiskEnhetOrgnr
+        );
+        rader = valgtEnhet[0].Underenheter.map((org, index) => {
             const liste: any = aktivStatusForRader;
             return (
-                <tr>
+                <tr key={org.OrganizationNumber}>
                     <td>
                         <Checkbox
                             label="Velg denne raden"
                             onChange={() => {
                                 liste[index] = !aktivStatusForRader[index];
                                 console.log(liste);
-
                                 setAktivStatusForRader(liste);
                                 console.log(aktivStatusForRader);
                             }}
@@ -51,7 +62,7 @@ const AntallBerorte: FunctionComponent = () => {
                     <td>{org.Name}</td>
                     <td>{org.OrganizationNumber}</td>
 
-                    {liste[index] && (
+                    {!liste[index] && (
                         <td>
                             <Input placeholder={'Fyll inn antall'} />
                         </td>
@@ -74,25 +85,34 @@ const AntallBerorte: FunctionComponent = () => {
                 lagre={async () => await context.lagre()}
                 slett={async () => await context.avbryt()}
             >
-                <table className="tabell">
-                    <thead>
-                        <tr>
-                            <th>
-                                <Checkbox label="Velg alle" />
-                            </th>
-                            <th>
-                                <Lenke href="#">Virksomhet</Lenke>
-                            </th>
-                            <th>
-                                <Lenke href="#">Bedriftsnummer</Lenke>
-                            </th>
-                            <th>
-                                <Lenke href="#">Antall berørte</Lenke>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>{rader}</tbody>
-                </table>
+                <div className={'hvem-berores'}>
+                    <BedriftsVelger
+                        className={'enhetsvelger'}
+                        setOrganisasjon={setJuridiskEnhetOrgnr}
+                        organisasjoner={
+                            organisasjonstre ? organisasjonstre.map(org => org.JuridiskEnhet) : []
+                        }
+                    />
+                    <table className="tabell">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <Checkbox label="Velg alle" />
+                                </th>
+                                <th>
+                                    <Lenke href="#">Virksomhet</Lenke>
+                                </th>
+                                <th>
+                                    <Lenke href="#">Bedriftsnummer</Lenke>
+                                </th>
+                                <th>
+                                    <Lenke href="#">Antall berørte</Lenke>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>{rader}</tbody>
+                    </table>
+                </div>
             </SkjemaRamme>
         </>
     );
