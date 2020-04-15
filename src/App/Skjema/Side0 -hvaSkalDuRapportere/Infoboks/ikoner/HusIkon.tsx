07abdcba8c5f2a5039995afaeff93a36d1e0f510
Terese Haug <terese.haug@nav.no>
2020-04-15 10:19:43 +0200

    HvaSkalDuRapportere - ny infoboks og tekst i radiopanel

---
+++ src/App/Skjema/Side0 -hvaSkalDuRapportere/Infoboks/ikoner/HusIkon.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const HusIkon = () => {
+    return (
+        <svg
+            className="infoboks-ikon"
+            width="18"
+            height="24"
+            viewBox="0 0 18 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M17 3H1V18H17V3Z" fill="#F8D4A1" />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M1 2.00001L2.64901 0.399414H15.3299L17 2.00001V3.00001H1V2.00001Z"
+                fill="#729CB2"
+            />
+            <mask
+                id="mask0"
+                mask-type="alpha"
+                maskUnits="userSpaceOnUse"
+                x="0"
+                y="0"
+                width="18"
+                height="24"
+            >
+                <path
+                    d="M1.707 2L2.707 1H15.293L16.293 2H1.707ZM16 7H13V4H16V7ZM16 12H13V9H16V12ZM16 17H13V14H16V17ZM11 7H7V4H11V7ZM11 12H7V9H11V12ZM11 17H7V14H11V17ZM5 7H2V4H5V7ZM5 12H2V9H5V12ZM5 17H2V14H5V17ZM15.853 0.146C15.76 0.053 15.633 0 15.5 0H2.5C2.367 0 2.24 0.053 2.146 0.146L0.146 2.146C0.145 2.146 0.145 2.148 0.144 2.148C0.052 2.243 0 2.394 0 2.5V23.5C0 23.776 0.224 24 0.5 24H7V19.5C7 19.224 7.224 19 7.5 19H10.5C10.776 19 11 19.224 11 19.5V24H17.5C17.776 24 18 23.776 18 23.5V2.5C18 2.395 17.946 2.239 17.854 2.148L15.853 0.146Z"
+                    fill="white"
+                />
+            </mask>
+            <g mask="url(#mask0)">
+                <path
+                    d="M1.707 2L2.707 1H15.293L16.293 2H1.707ZM16 7H13V4H16V7ZM16 12H13V9H16V12ZM16 17H13V14H16V17ZM11 7H7V4H11V7ZM11 12H7V9H11V12ZM11 17H7V14H11V17ZM5 7H2V4H5V7ZM5 12H2V9H5V12ZM5 17H2V14H5V17ZM15.853 0.146C15.76 0.053 15.633 0 15.5 0H2.5C2.367 0 2.24 0.053 2.146 0.146L0.146 2.146C0.145 2.146 0.145 2.148 0.144 2.148C0.052 2.243 0 2.394 0 2.5V23.5C0 23.776 0.224 24 0.5 24H7V19.5C7 19.224 7.224 19 7.5 19H10.5C10.776 19 11 19.224 11 19.5V24H17.5C17.776 24 18 23.776 18 23.5V2.5C18 2.395 17.946 2.239 17.854 2.148L15.853 0.146Z"
+                    fill="#24536F"
+                />
+                <path d="M18 0H0V2H18V0Z" fill="#729CB2" />
+            </g>
+        </svg>
+    );
+};
+export default HusIkon;
