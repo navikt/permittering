5c1e4d3af883c46ad1ddc8978b1af01f84b60378
Mats Bolstad <mbolstad@gmail.com>
2020-05-05 14:25:13 +0200

    Navn på toggle ble vist i teksten (#346)

--- src/App/IkkeTilgang/IkkeTilgang.tsx
+++ src/App/IkkeTilgang/IkkeTilgang.tsx
@@ -1,25 +1,19 @@
-import { FunctionComponent, useContext } from 'react';
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import HvitSideBoks from '../komponenter/HvitSideBoks';
 import { Undertittel } from 'nav-frontend-typografi';
-import { Feature, FeatureToggleContext } from '../FeatureToggleProvider';
 import { BrodsmuleSti } from '../komponenter/BrodsmuleSti/BrodsmuleSti';
 
-const IkkeTilgang: FunctionComponent = props => {
-    const featureToggleContext = useContext(FeatureToggleContext);
-    const visskjema = featureToggleContext[Feature.visskjema];
+const IkkeTilgang: FunctionComponent = () => {
     return (
         <HvitSideBoks>
             <BrodsmuleSti />
             <Undertittel>
                 Du har dessverre ikke tilgang til å fylle ut skjema om permittering
             </Undertittel>
-            <br />
-            <>
+            <p>
                 For å fylle ut skjema kreves det at du har en rolle eller rettighet i bedriften i
                 Altinn
-                {visskjema && <>visskjema</>}
-            </>
+            </p>
         </HvitSideBoks>
     );
 };
