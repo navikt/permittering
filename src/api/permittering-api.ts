a6ad7909682aad3b0b8ba3c907ca18fbd45f2ef1
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-06-16 16:35:23 +0200

    lagt til ny lenke for a sjekke innlogging

--- src/api/permittering-api.ts
+++ src/api/permittering-api.ts
@@ -1,6 +1,21 @@
 import { OpprettSkjema, Permitteringsskjema } from '../types/permitteringsskjema';
-import { skjemaAvbrytPath, skjemaListPath, skjemaPath, skjemaSendInnPath } from '../paths.json';
+import {
+    sjekkInnloggetLink,
+    skjemaAvbrytPath,
+    skjemaListPath,
+    skjemaPath,
+    skjemaSendInnPath,
+} from '../paths.json';
 import api from './api-client';
+
+export async function sjekkInnlogget(signal: any): Promise<boolean> {
+    let respons = await fetch(sjekkInnloggetLink, { signal: signal });
+    if (respons.ok) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 export const hent = async (id: string) => {
     const url = skjemaPath.replace(':id', id);
