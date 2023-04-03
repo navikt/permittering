fcfa881e582eef359e5c526973a1d9cc51078956
Ken Gullaksen <ken.gullaksen@nav.no>
2023-04-03 11:06:51 +0200

    try to fix http 504 in dev to pam-janzz

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -18,7 +18,7 @@ const getConfiguredRouter = (tokenXClient, tokenXIssuer) => {
     indexRoute(app);
     app.use(
         paths.stillingstitlerPath,
-        proxy('http://pam-janzz.teampam', {
+        proxy('http://pam-janzz.teampam.svc.nais.local', {
             proxyReqPathResolver: (req) => {
                 return `/pam-janzz/rest/typeahead/yrke-med-styrk08-nav${req.url}`;
             },
