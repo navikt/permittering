b232a92456e3726a4d4bfd4dfe49d400ae7cf2ad
Ken Gullaksen <ken.gullaksen@nav.no>
2023-04-17 10:13:50 +0200

    rm react-intl

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,5 @@
 import 'moment/locale/nb';
 import React, { useEffect } from 'react';
-import { IntlProvider } from 'react-intl';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { brukerLoggetPa } from '../utils/funksjonerForAmplitudeLogging';
 import Forside from './Forside/Forside';
@@ -23,51 +22,49 @@ function App() {
     const basePath = '/permittering';
 
     return (
-        <IntlProvider locale={'nb'}>
-            <div className="app">
-                <LoginBoundary>
-                    <Router basename={basePath}>
-                        <Route exact path="/">
-                            <Forside />
-                        </Route>
-                        <Route path="/skjema">
-                            <OrganisasjonsListeProvider>
-                                <Route exact path="/skjema/start">
-                                    <SkjemaProvider>
-                                        <Side0 />
-                                    </SkjemaProvider>
-                                </Route>
-                                <Route exact path="/skjema/kontaktinformasjon/:id">
-                                    <SkjemaProvider>
-                                        <Side1 />
-                                    </SkjemaProvider>
-                                </Route>
-                                <Route exact path="/skjema/generelle-opplysninger/:id">
-                                    <SkjemaProvider>
-                                        <Side2 />
-                                    </SkjemaProvider>
-                                </Route>
-                                <Route exact path="/skjema/yrkeskategorier/:id">
-                                    <SkjemaProvider>
-                                        <Side5 />
-                                    </SkjemaProvider>
-                                </Route>
-                                <Route exact path="/skjema/oppsummering/:id">
-                                    <SkjemaProvider>
-                                        <Oppsummering />
-                                    </SkjemaProvider>
-                                </Route>
-                                <Route exact path="/skjema/kvitteringsside/:id">
-                                    <SkjemaProvider>
-                                        <Kvitteirng />
-                                    </SkjemaProvider>
-                                </Route>
-                            </OrganisasjonsListeProvider>
-                        </Route>
-                    </Router>
-                </LoginBoundary>
-            </div>
-        </IntlProvider>
+        <div className="app">
+            <LoginBoundary>
+                <Router basename={basePath}>
+                    <Route exact path="/">
+                        <Forside />
+                    </Route>
+                    <Route path="/skjema">
+                        <OrganisasjonsListeProvider>
+                            <Route exact path="/skjema/start">
+                                <SkjemaProvider>
+                                    <Side0 />
+                                </SkjemaProvider>
+                            </Route>
+                            <Route exact path="/skjema/kontaktinformasjon/:id">
+                                <SkjemaProvider>
+                                    <Side1 />
+                                </SkjemaProvider>
+                            </Route>
+                            <Route exact path="/skjema/generelle-opplysninger/:id">
+                                <SkjemaProvider>
+                                    <Side2 />
+                                </SkjemaProvider>
+                            </Route>
+                            <Route exact path="/skjema/yrkeskategorier/:id">
+                                <SkjemaProvider>
+                                    <Side5 />
+                                </SkjemaProvider>
+                            </Route>
+                            <Route exact path="/skjema/oppsummering/:id">
+                                <SkjemaProvider>
+                                    <Oppsummering />
+                                </SkjemaProvider>
+                            </Route>
+                            <Route exact path="/skjema/kvitteringsside/:id">
+                                <SkjemaProvider>
+                                    <Kvitteirng />
+                                </SkjemaProvider>
+                            </Route>
+                        </OrganisasjonsListeProvider>
+                    </Route>
+                </Router>
+            </LoginBoundary>
+        </div>
     );
 }
 
