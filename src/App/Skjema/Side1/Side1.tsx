74df5850033db943e3a018bc5edfca019cffa314
Ken Gullaksen <ken.gullaksen@nav.no>
2023-04-26 09:40:22 +0200

    fiks og forenkle layout i kontaktinfo

--- src/App/Skjema/Side1/Side1.tsx
+++ src/App/Skjema/Side1/Side1.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button, Heading, TextField } from '@navikt/ds-react';
+import { BodyShort, Button, Heading, Label, TextField } from '@navikt/ds-react';
 import SkjemaContext from '../SkjemaContext/SkjemaContext';
 import SkjemaRamme from '../../komponenter/SkjemaRamme';
 import { useSkjemaSteg } from '../use-skjema-steg';
@@ -25,8 +25,6 @@ const Side1: FunctionComponent = () => {
         history.replace('/');
     }
 
-    console.log({ nesteSide });
-
     return (
         <>
             <Dekorator sidetittel={context.skjema.type} />
@@ -38,73 +36,82 @@ const Side1: FunctionComponent = () => {
                 <Heading level="2" size="medium" spacing>
                     Kontaktinformasjon
                 </Heading>
-                <Heading level="3" size="small" spacing>
-                    Informasjon om arbeidsgiver
-                </Heading>
+                <div className="skjema-innhold__side-1-seksjon">
+                    <Heading level="3" size="small" spacing>
+                        Informasjon om arbeidsgiver
+                    </Heading>
+                    <div className="skjema-innhold__side-1-felt__sbs">
+                        <div className="skjema-innhold__side-1-felt">
+                            <Label spacing>Virksomhetsnavn</Label>
+                            <BodyShort size="small" spacing>
+                                {context.skjema.bedriftNavn}
+                            </BodyShort>
+                        </div>
+                        <div className="skjema-innhold__side-1-felt">
+                            <Label spacing>Virksomhetsnummer</Label>
+                            <BodyShort size="small" spacing>
+                                {context.skjema.bedriftNr}
+                            </BodyShort>
+                        </div>
+                    </div>
+                </div>
+                <div className="skjema-innhold__side-1-seksjon">
+                    <Heading level="3" size="small" spacing>
+                        Kontaktperson i virksomheten
+                    </Heading>
+                    <TextField
+                        className="skjema-innhold__side-1-felt"
+                        label="Navn"
+                        defaultValue={context.skjema.kontaktNavn}
+                        onChange={(event) =>
+                            context.endreSkjemaVerdi('kontaktNavn', event.currentTarget.value)
+                        }
+                    />
+                    <div className="skjema-innhold__side-1-felt__sbs">
+                        <TextField
+                            className="skjema-innhold__side-1-felt"
+                            label="Telefonnummer"
+                            defaultValue={context.skjema.kontaktTlf}
+                            error={feilMeldingTelefonNr}
+                            onBlur={(event: any) => {
+                                if (erGyldigTelefonNr(event.currentTarget.value)) {
+                                    const telefonNummer = event.currentTarget.value;
+                                    const riktigFormat = telefonNummer.substr(
+                                        telefonNummer.length - 8,
+                                        telefonNummer.length
+                                    );
+                                    context.endreSkjemaVerdi('kontaktTlf', riktigFormat);
+                                    setFeilmeldingTelefonNr('');
+                                } else
+                                    setFeilmeldingTelefonNr(
+                                        'Vennligst oppgi et gyldig telefonnummer'
+                                    );
+                            }}
+                            onChange={() => setFeilmeldingTelefonNr('')}
+                        />
 
-                <div className="skjema-innhold__side-1-linje-2">
-                    <TextField
-                        className={'skjema-innhold__side-1-input-felt'}
-                        label="Bedriftens navn"
-                        defaultValue={context.skjema.bedriftNavn}
-                        disabled
-                    />
-                    <TextField
-                        className={'skjema-innhold__side-1-input-felt'}
-                        label="Bedriftsnummer"
-                        defaultValue={context.skjema.bedriftNr}
-                        disabled
-                    />
+                        <TextField
+                            className={'skjema-innhold__side-1-felt'}
+                            label="E-post"
+                            defaultValue={context.skjema.kontaktEpost}
+                            error={feilMeldingEpost}
+                            onBlur={(event) => {
+                                if (erGyldigEpost(event.currentTarget.value)) {
+                                    context.endreSkjemaVerdi(
+                                        'kontaktEpost',
+                                        event.currentTarget.value
+                                    );
+                                    setFeilmeldingEpost('');
+                                } else {
+                                    setFeilmeldingEpost('Vennligst oppgi en gyldig e-post');
+                                }
+                            }}
+                            onChange={() => setFeilmeldingEpost('')}
+                        />
+                    </div>
                 </div>
-                <Heading level="3" size="small" spacing>
-                    Kontaktperson i virksomheten
-                </Heading>
-                <TextField
-                    className={'skjema-innhold__side-1-input-felt'}
-                    label="Navn"
-                    defaultValue={context.skjema.kontaktNavn}
-                    onChange={(event) =>
-                        context.endreSkjemaVerdi('kontaktNavn', event.currentTarget.value)
-                    }
-                />
-                <div className={'skjema-innhold__side-1-linje-4'}>
-                    <TextField
-                        className={'skjema-innhold__side-1-input-felt'}
-                        label="Telefonnummer"
-                        defaultValue={context.skjema.kontaktTlf}
-                        error={feilMeldingTelefonNr}
-                        onBlur={(event: any) => {
-                            if (erGyldigTelefonNr(event.currentTarget.value)) {
-                                const telefonNummer = event.currentTarget.value;
-                                const riktigFormat = telefonNummer.substr(
-                                    telefonNummer.length - 8,
-                                    telefonNummer.length
-                                );
-                                context.endreSkjemaVerdi('kontaktTlf', riktigFormat);
-                                setFeilmeldingTelefonNr('');
-                            } else
-                                setFeilmeldingTelefonNr('Vennligst oppgi et gyldig telefonnummer');
-                        }}
-                        onChange={() => setFeilmeldingTelefonNr('')}
-                    />
-                    <TextField
-                        className={'skjema-innhold__side-1-input-felt'}
-                        label="E-post"
-                        defaultValue={context.skjema.kontaktEpost}
-                        error={feilMeldingEpost}
-                        onBlur={(event) => {
-                            if (erGyldigEpost(event.currentTarget.value)) {
-                                context.endreSkjemaVerdi('kontaktEpost', event.currentTarget.value);
-                                setFeilmeldingEpost('');
-                            } else {
-                                setFeilmeldingEpost('Vennligst oppgi en gyldig e-post');
-                            }
-                        }}
-                        onChange={() => setFeilmeldingEpost('')}
-                    />
-                </div>
-                <div className={'skjema-innhold__fram-og-tilbake'}>
-                    &nbsp;
+                <div className="skjema-innhold__fram-og-tilbake">
+                    <span>{/*hacks button to align right*/}</span>
                     <Button
                         onClick={async () => {
                             await context.lagre();
