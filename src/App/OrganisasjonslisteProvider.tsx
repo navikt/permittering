1b299eecd9c3cd2e3a075930b7f7ab7ecfec62fa
Ken Gullaksen <ken.gullaksen@nav.no>
2023-12-06 14:02:57 +0100

    legg på skeletons og feilmelding

--- src/App/OrganisasjonslisteProvider.tsx
+++ src/App/OrganisasjonslisteProvider.tsx
@@ -6,7 +6,7 @@ import {z} from "zod";
 import * as Sentry from "@sentry/browser";
 import {Side} from "./Side";
 import {Breadcrumbs} from "./Skjema/Breadcrumbs";
-import {Alert, Box, Heading, Skeleton, VStack} from "@navikt/ds-react";
+import {BodyLong, BodyShort, Box, Heading, Link, List, Skeleton, VStack} from "@navikt/ds-react";
 
 export type OrganisajonsContext = {
     organisasjoner: Array<Organisasjon>;
@@ -28,9 +28,8 @@ export const OrganisasjonsListeProvider: FunctionComponent<PropsWithChildren> = 
         }
     }, [organisasjoner, organisasjon, setOrganisasjon]);
 
-
     if (isError) {
-        return <AltinnFeil />;
+        return <Feilside />;
     }
 
     if (organisasjoner === undefined || organisasjon === undefined) {
@@ -65,15 +64,33 @@ const Laster: FunctionComponent = () => {
                 padding={{xs: '2', md: '4', lg: '8'}}
             >
                 <VStack gap="12">
-                    <Skeleton variant="rectangle" width="100%" height={30} />
-                    <Skeleton variant="rectangle" width="100%" height={30} />
+                    <VStack gap="4">
+                        <BodyLong as={Skeleton} size="medium">
+                            Skal du permittere, si opp eller innskrenke arbeidstiden til 10 eller flere ansatte?
+                            Da har du meldeplikt til NAV. Du kan også melde fra til NAV hvis det gjelder under 10 ansatte, om ønskelig.
+                        </BodyLong>
+
+                        <Skeleton width="20vw" height="3rem" />
+                    </VStack>
+
+
+                    <VStack gap="4">
+                        <Heading as={Skeleton} level="2" size="large">
+                            Velg aktuelt skjema:
+                        </Heading>
+
+                        <Skeleton variant="rectangle" width="100%" height="8vh" />
+                        <Skeleton variant="rectangle" width="100%" height="8vh" />
+                        <Skeleton variant="rectangle" width="100%" height="8vh" />
+                    </VStack>
                 </VStack>
+
             </Box>
         </Side>
     );
 };
 
-const AltinnFeil: FunctionComponent = () => {
+const Feilside: FunctionComponent = () => {
     return (
         <Side
             tittel="Skjema til NAV om permitteringer, oppsigelser, eller innskrenkning i arbeidstid"
@@ -84,14 +101,25 @@ const AltinnFeil: FunctionComponent = () => {
                 borderRadius="small"
                 padding={{xs: '2', md: '4', lg: '8'}}
             >
-                <VStack gap="12">
-                    <Heading level="2" size="small">
-                        Henting av tilganger feilet
-                    </Heading>
-                    <Alert variant="error">
-                        Det oppstod en feil ved henting av tilganger. Prøv igjen om noen minutter.
-                    </Alert>
-                </VStack>
+                <Heading level="1" size="large" spacing>
+                    Henting av tilganger feilet
+                </Heading>
+                <BodyShort spacing>
+                    En teknisk feil på våre servere gjør at siden er utilgjengelig.
+                    Dette skyldes ikke noe du gjorde.
+                </BodyShort>
+                <BodyShort>Du kan prøve å</BodyShort>
+                <List>
+                    <List.Item>vente noen minutter og laste siden på nytt</List.Item>
+                    <List.Item>gå tilbake til forrige side</List.Item>
+                </List>
+                <BodyShort>
+                    Hvis problemet vedvarer, kan du{" "}
+                    <Link href="https://nav.no/kontaktoss" target="_blank">
+                        kontakte oss (åpnes i ny fane)
+                    </Link>
+                    .
+                </BodyShort>
             </Box>
         </Side>
     );
--- src/App/Skjema/Kvittering.tsx
+++ src/App/Skjema/Kvittering.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from "react";
 import {useParams} from "react-router-dom";
-import {Alert, Box, Heading, HStack, LinkPanel, VStack} from "@navikt/ds-react";
+import {Alert, Box, Heading, HStack, LinkPanel, Skeleton, VStack} from "@navikt/ds-react";
 import {Oppsummeringsfelter} from "../komponenter/Oppsummeringsfelter";
 import {FileCheckmarkFillIcon} from "@navikt/aksel-icons";
 import {useHentSkjema} from "../../api/permittering-api";
@@ -12,7 +12,7 @@ export const Kvittering: FunctionComponent = () => {
     const {skjemaId} = useParams();
     const {skjema, error} = useHentSkjema(skjemaId);
 
-    if (!skjema || error) {
+    if (error) {
         return <Side tittel="Kvitteringsside">
             <Breadcrumbs />
             <Box
@@ -22,6 +22,51 @@ export const Kvittering: FunctionComponent = () => {
             >
                 <Alert variant="error">Klarte ikke hente skjema akkurat nå! Prøv igjen om noen minutter.</Alert>
             </Box>
+        </Side>
+    }
+
+    if (!skjema) {
+        return <Side tittel="Laster kvittering">
+            <Breadcrumbs />
+            <VStack gap="4">
+                <HStack align="center">
+                    <FileCheckmarkFillIcon title="a11y-title" fontSize="4rem" color="var(--a-green-500)"/>
+                    <Heading as={Skeleton} size="large" level="2">Kvittering på mottatt melding</Heading>
+                </HStack>
+                <Box
+                    background="bg-default"
+                    borderRadius="small"
+                    padding={{xs: '2', md: '4', lg: '8'}}
+                >
+                    <VStack gap="4">
+                        <Skeleton variant="rectangle" width="100%" />
+                        <Skeleton variant="rectangle" width="100%" />
+                        <div className="oppsummering_linje"/>
+
+                        <Skeleton variant="rectangle" width="100%" />
+                        <Skeleton variant="rectangle" width="100%" />
+                        <Skeleton variant="rectangle" width="100%" />
+                        <div className="oppsummering_linje"/>
+
+                        <Skeleton variant="rectangle" width="100%" />
+                        <div className="oppsummering_linje"/>
+
+                        <Skeleton variant="rectangle" width="100%" />
+                        <div className="oppsummering_linje"/>
+
+                        <Skeleton variant="rectangle" width="100%" />
+                        <div className="oppsummering_linje"/>
+
+                        <Skeleton variant="rectangle" width="100%" />
+                        <div className="oppsummering_linje"/>
+
+                        <Skeleton variant="rectangle" width="100%" height="8vh" />
+                    </VStack>
+                </Box>
+
+                <Skeleton variant="rectangle" width="100%" height="8vh" />
+                <Skeleton variant="rectangle" width="100%" height="8vh" />
+            </VStack>
         </Side>
     }
 
