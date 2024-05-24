ad6ba04bbe10c8b86ce5ad2f5c475dc62a30080e
Ken Gullaksen <ken.gullaksen@nav.no>
2024-05-24 13:52:36 +0200

    flytt demo til ansatt domene

--- src/App/Skjema/Breadcrumbs.tsx
+++ src/App/Skjema/Breadcrumbs.tsx
@@ -1,26 +1,25 @@
-import {FunctionComponent, useEffect} from "react";
-import {setBreadcrumbs} from "@navikt/nav-dekoratoren-moduler";
-import {gittMiljo} from "../../utils/environment";
-
+import { FunctionComponent, useEffect } from 'react';
+import { setBreadcrumbs } from '@navikt/nav-dekoratoren-moduler';
+import { gittMiljo } from '../../utils/environment';
 
 const urlTilMinSideArbeidsgiver = gittMiljo({
     prod: 'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/',
     other: 'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/',
     dev: 'https://arbeidsgiver.intern.dev.nav.no/min-side-arbeidsgiver/',
-    demo: 'https://arbeidsgiver.ekstern.dev.nav.no/min-side-arbeidsgiver/',
+    demo: 'https://arbeidsgiver.ansatt.dev.nav.no/min-side-arbeidsgiver/',
 });
 
 const urlTilPermittering = gittMiljo({
     prod: 'https://arbeidsgiver.nav.no/permittering',
     other: 'https://arbeidsgiver.nav.no/permittering',
     dev: 'https://permitteringsskjema.intern.dev.nav.no/permittering',
-    demo: 'https://arbeidsgiver.ekstern.dev.nav.no/permittering/',
+    demo: 'https://arbeidsgiver.ansatt.dev.nav.no/permittering/',
 });
 
 const defaultBreadcrumbs = [
     { url: urlTilMinSideArbeidsgiver, title: 'Min side – arbeidsgiver' },
     { url: urlTilPermittering, title: 'Permittering og oppsigelse' },
-]
+];
 
 export type Breadcrumb = {
     url: string;
@@ -28,13 +27,10 @@ export type Breadcrumb = {
     handleInApp?: boolean;
 };
 
-export const Breadcrumbs: FunctionComponent<{ breadcrumb?: Breadcrumb}> = ({ breadcrumb }) => {
+export const Breadcrumbs: FunctionComponent<{ breadcrumb?: Breadcrumb }> = ({ breadcrumb }) => {
     useEffect(() => {
-        setBreadcrumbs([
-            ...defaultBreadcrumbs,
-            ...(breadcrumb ? [breadcrumb] : []),
-        ]);
+        setBreadcrumbs([...defaultBreadcrumbs, ...(breadcrumb ? [breadcrumb] : [])]);
     }, [breadcrumb]);
 
     return <></>;
-}
+};
