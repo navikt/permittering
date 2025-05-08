25e27e170abc73e1180c800de0cf4c041c5f3d2d
Jonas Enge <jonas.maccyber.enge@nav.no>
2025-05-08 12:30:00 +0200

    Fjerner amplitude. Logger via dekoratøren

--- src/App/Forside/Forside.tsx
+++ src/App/Forside/Forside.tsx
@@ -1,19 +1,17 @@
-import React, {FunctionComponent} from 'react';
-import {VStack} from '@navikt/ds-react';
+import React, { FunctionComponent } from 'react';
+import { VStack } from '@navikt/ds-react';
 import InfoOmMeldepliktBoks from './InfoOmMeldepliktBoks/InfoOmMeldepliktBoks';
 import './Forside.css';
-import {InnsendteSkjemaer} from "./InnsendteSkjema/InnsendteSkjemaer";
-import {Side} from "../Side";
-import {Breadcrumbs} from "../Skjema/Breadcrumbs";
+import { InnsendteSkjemaer } from './InnsendteSkjema/InnsendteSkjemaer';
+import { Side } from '../Side';
+import { Breadcrumbs } from '../Skjema/Breadcrumbs';
 
 const Forside: FunctionComponent = () => {
     return (
-        <Side
-            tittel="Skjema til NAV om permitteringer, oppsigelser, eller innskrenkning i arbeidstid"
-        >
+        <Side tittel="Skjema til NAV om permitteringer, oppsigelser, eller innskrenkning i arbeidstid">
             <Breadcrumbs />
             <VStack gap="8" className="forside-container">
-                <InfoOmMeldepliktBoks/>
+                <InfoOmMeldepliktBoks />
                 <InnsendteSkjemaer />
             </VStack>
         </Side>
