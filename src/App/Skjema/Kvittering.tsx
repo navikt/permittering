67823503dcffbe32d5a542365ab5077b2efdf71e
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2025-08-06 12:49:35 +0200

    legg til nye knapper for aa-reg

--- src/App/Skjema/Kvittering.tsx
+++ src/App/Skjema/Kvittering.tsx
@@ -1,13 +1,26 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Alert, Box, FormSummary, LinkPanel, Skeleton, VStack } from '@navikt/ds-react';
+import {
+    Alert,
+    BodyLong,
+    Box,
+    Button,
+    ExpansionCard,
+    FormSummary,
+    HStack,
+    Link,
+    LinkPanel,
+    Skeleton,
+    Textarea,
+    VStack,
+} from '@navikt/ds-react';
 import { Oppsummeringsfelter } from '../komponenter/Oppsummeringsfelter';
 import { useHentSkjema } from '../../api/permittering-api';
 import { Breadcrumbs } from './Breadcrumbs';
 import { Side } from '../Side';
 import { sidetitler } from './Skjema';
 import { logger } from '../../utils/analytics';
-
+import { SkjemaType } from '../../types/Permitteringsskjema';
 
 export const Kvittering: FunctionComponent = () => {
     const { skjemaId } = useParams();
@@ -48,6 +61,83 @@ export const Kvittering: FunctionComponent = () => {
             />
             <VStack gap="8">
                 <Oppsummeringsfelter skjema={skjema} tittel="Innsendte opplysninger" />
+                {skjema.type === SkjemaType.enum.PERMITTERING_UTEN_LØNN && (
+                    <>
+                        <ExpansionCard aria-label={'Husk å rapportere permittering i A-meldingen'}>
+                            <ExpansionCard.Header>
+                                <ExpansionCard.Title>
+                                    Husk å rapportere permittering i A-meldingen
+                                </ExpansionCard.Title>
+                                <ExpansionCard.Description>
+                                    Opplysningene i A-meldingen oppdaterer Aa-registeret, som brukes
+                                    av NAV og andre offentlige etater. Feil informasjon kan gi feil
+                                    vurderinger.
+                                </ExpansionCard.Description>
+                            </ExpansionCard.Header>
+                            <ExpansionCard.Content>
+                                <HStack gap="8">
+                                    <Button
+                                        variant="tertiary"
+                                        onClick={() =>
+                                            (window.location.href =
+                                                'https://www.skatteetaten.no/bedrift-og-organisasjon/arbeidsgiver/a-meldingen/veiledning/arbeidsforholdet/opplysninger-om-arbeidsforholdet/permittering/')
+                                        }
+                                    >
+                                        Slik rapporterer du
+                                    </Button>
+                                    <Button
+                                        variant="secondary"
+                                        onClick={() =>
+                                            (window.location.href =
+                                                'https://arbeidsgiver.nav.no/arbeidsforhold/')
+                                        }
+                                    >
+                                        Kontrollere info i Aa-reg
+                                    </Button>
+                                </HStack>
+                            </ExpansionCard.Content>
+                        </ExpansionCard>
+                        <ExpansionCard aria-label={'Del informasjon til berørte ansatte'}>
+                            <ExpansionCard.Header>
+                                <ExpansionCard.Title>
+                                    Del informasjon til berørte ansatte
+                                </ExpansionCard.Title>
+                                <ExpansionCard.Description>
+                                    Her finner du nyttig informasjon om hvordan man søker dagpenger,
+                                    sender meldekort og registrer seg som arbeidssøker.
+                                </ExpansionCard.Description>
+                            </ExpansionCard.Header>
+                            <ExpansionCard.Content>
+                                <VStack gap="8">
+                                    <BodyLong>
+                                        Denne filmen viser vi hva det vil si å være permittert,
+                                        plikter og rettigheter:{' '}
+                                        <span>
+                                            <Link
+                                                href={
+                                                    'https://player.vimeo.com/video/566520073?h=79f8d36395%22'
+                                                }
+                                            >
+                                                https://player.vimeo.com/video/566520073?h=79f8d36395%22
+                                            </Link>
+                                        </span>
+                                    </BodyLong>
+                                    <BodyLong>
+                                        Her finner du mye god skriftlig informasjon om det å bli
+                                        permittert eller nedbemanne:{' '}
+                                        <span>
+                                            <Link
+                                                href={'https://www.nav.no/arbeidsledig-permittert'}
+                                            >
+                                                Er arbeidsledig eller permittert - nav.no
+                                            </Link>
+                                        </span>
+                                    </BodyLong>
+                                </VStack>
+                            </ExpansionCard.Content>
+                        </ExpansionCard>
+                    </>
+                )}
                 <LinkPanel
                     href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver"
                     onClick={() => {
@@ -141,8 +231,7 @@ const KvitteringLaster = () => (
                                     </FormSummary.Value>
                                 </FormSummary.Answer>
                                 <FormSummary.Answer>
-                                    <FormSummary.Label>
-                                    </FormSummary.Label>
+                                    <FormSummary.Label></FormSummary.Label>
                                     <FormSummary.Value>
                                         <Skeleton variant="rectangle" width="100%" />
                                     </FormSummary.Value>
@@ -154,8 +243,7 @@ const KvitteringLaster = () => (
                                     </FormSummary.Value>
                                 </FormSummary.Answer>
                                 <FormSummary.Answer>
-                                    <FormSummary.Label>
-                                    </FormSummary.Label>
+                                    <FormSummary.Label></FormSummary.Label>
                                     <FormSummary.Value>
                                         <Skeleton variant="rectangle" width="100%" />
                                     </FormSummary.Value>
